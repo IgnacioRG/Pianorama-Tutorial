aec72afd3de2dabbeca4b08dc13f0a9a008e07e9
IgnacioRG <IgnacioRG@ciencias.unam.mx>
2023-12-04 11:22:50 -0600

    Pianorama Archive
    Archivos originales del juego de Pianorama antes de la implementación del tutorial.

---
+++ ARCHIVE/Pianorama/PianoScripts/MenuPiano.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuPiano : MonoBehaviour
+{
+    public void Jugar() {
+        SceneManager.LoadScene("Piano");        
+    }
+
+    public void Salir() {
+        SceneManager.LoadScene("Menu Juegos");
+    }
+}
